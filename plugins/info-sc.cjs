e437295d758f63e519266162bdca8abc30c61b98
Nizar Rahman <102470420+nizarrahman@users.noreply.github.com>
2023-01-04 06:58:32 +0700

    Update info-sc.cjs

--- plugins/info-sc.cjs
+++ plugins/info-sc.cjs
@@ -1,6 +1,6 @@
 var handler = async(m, { conn, text, usedPrefix, command }) => {
 
-var str = `*https://github.com/FahriAdison/Sad-Bot-V2*\nSubscribe Untuk Mengetahui Update Selanjutnya:bit.ly/Papah-Chan\nSilahkan follow github saya dibawah ,Terimakasih`
+var str = `*Kamu Nanya *\n\nSilahkan follow github saya dibawah ,Terimakasih`
 conn.sendButtonDoc(m.chat, str, wm, 'Thanks','Bilek', ftextt, m)
 }
 handler.command = handler.help = ['sc']
